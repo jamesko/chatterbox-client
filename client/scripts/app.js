33be84145927d9e7d866cf56c07b8969a2571fa1
James Kolonusz <jamesak@gmail.com>
2014-12-29 21:10:12 -0800

    Add code to fetch messages, add messages and mitigate XSS attacks.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,21 @@ var app = {};
 
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-app.init = function(){};
+app.init = function(){
+  this.friends = [];
+  $(".username").on("click", function(){
+    app.addFriend($(this).text());    
+    });
+
+  $("#send").on("submit", function(){ 
+    app.handleSubmit();
+    return false;
+  });
+
+  setInterval(app.fetch, 5000);
+
+};
+
 
 app.send = function(message){
   $.ajax({
@@ -27,12 +41,22 @@ app.fetch = function(){
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data : {'order':'-createdAt'},
     success: function (data) {
-
+      var results = data.results;      
+      var message = {};
+      for(var i=0;i<results.length;i++){
+        message = {
+                   username: results[i].username,
+                   text: results[i].text,
+                   room: results[i].roomname
+                 }
+         app.addMessage(message);
+      }      
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+    console.error('chatterbox: Failed to send message');
     }
   });
 };
@@ -42,14 +66,28 @@ app.clearMessages = function(){
 };
 
 app.addMessage = function(message){
-  var $span = $("<span class='chat'>"+message.username+": "+message.text+"</span>");  
-  $("#chats").append($span);
+
+  var username = "<span class='username'>"+ 
+                  encodeURI(message.username)+"</span>";
+  var $div = $("<div class='chat'>"+username+": "+ 
+                encodeURI(message.text)+"</div>");
+  $("#chats").append($div);
 };
 
 app.addRoom = function(room){
   var $span = $("<span class='chat'>"+room+"</span>");  
   $("#roomSelect").append($span);  
 };
+
+app.addFriend = function(){
+  // ADD FRIENDS!!!!
+};
+
+app.handleSubmit = function(message){
+  app.send(message);
+};
+
+$(document).ready(function(){app.init()});
 
 
 
